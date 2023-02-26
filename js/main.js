0bf79dfe7fa94ef03c8d47088ea06ab3d8fe9733
Ayush Dayama <ayushdayama097@gmail.com>
2023-02-26 19:21:16 +0530

    Added hyperlink copying to clipboard

--- js/main.js
+++ js/main.js
@@ -1,96 +1,96 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
-	
-	var fullHeight = function() {
 
-		if ( !isMobile.any() ) {
+	var fullHeight = function () {
+
+		if (!isMobile.any()) {
 			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
+			$(window).resize(function () {
 				$('.js-fullheight').css('height', $(window).height());
 			});
 		}
 	};
 
 	// Parallax
-	var parallax = function() {
+	var parallax = function () {
 		$(window).stellar();
 	};
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight animated-fast');
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
+						}, k * 100, 'easeInOutExpo');
 					});
-					
+
 				}, 50);
-				
+
 			}
 
-		} , { offset: '85%' } );
+		}, { offset: '85%' });
 	};
 
 
 
-	var goToTop = function() {
+	var goToTop = function () {
 
-		$('.js-gotop').on('click', function(event){
-			
+		$('.js-gotop').on('click', function (event) {
+
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
-			
+
 			return false;
 		});
 
-		$(window).scroll(function(){
+		$(window).scroll(function () {
 
 			var $win = $(window);
 			if ($win.scrollTop() > 200) {
@@ -100,42 +100,42 @@
 			}
 
 		});
-	
+
 	};
 
-	var pieChart = function() {
+	var pieChart = function () {
 		$('.chart').easyPieChart({
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
 			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
+			trackColor: "#f5f5f5",
 			size: 160,
 			animate: 1000
 		});
 	};
 
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
+	var skillsWayPoint = function () {
+		if ($('#fh5co-skills').length > 0) {
+			$('#fh5co-skills').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout(pieChart, 400);
 					$(this.element).addClass('animated');
 				}
-			} , { offset: '90%' } );
+			}, { offset: '90%' });
 		}
 
 	};
 
 
 	// Loading page
-	var loaderPage = function() {
+	var loaderPage = function () {
 		$(".fh5co-loader").fadeOut("slow");
 	};
 
-	
-	$(function(){
+
+	$(function () {
 		contentWayPoint();
 		goToTop();
 		loaderPage();
@@ -144,18 +144,47 @@
 		// pieChart();
 		skillsWayPoint();
 	});
-
-
 }());
 
-function showMeAlert(sectionName) {
+function copyToClipboard(sectionName) {
 	var sectionURL = "";
-	if	   (sectionName == "aboutMe") 	 { sectionURL = "#fh5co-about";    }
-	else if(sectionName == "resume")  	 { sectionURL = "#fh5co-resume";   }
-	else if(sectionName == "certAndAch") { sectionURL = "#fh5co-features"; }
-	else if(sectionName == "skills")  	 { sectionURL = "#fh5co-skills";   }
-	else if(sectionName == "fmw")	  	 { sectionURL = "#fh5co-blog";     }
-	else if(sectionName == "hireMe")  	 { sectionURL = "#fh5co-started";  }
-	else if(sectionName == "contact") 	 { sectionURL = "#fh5co-consult";  }
-	window.prompt("Like this section? \nHere's the URL to reach me easily next time 😃", "https://ayushdayama.github.io/Portfolio" + sectionURL);
+	if (sectionName == "aboutMe") {
+		sectionURL = "#fh5co-about";
+	} else if (sectionName == "resume") {
+		sectionURL = "#fh5co-resume";
+	} else if (sectionName == "certAndAch") {
+		sectionURL = "#fh5co-features";
+	} else if (sectionName == "skills") {
+		sectionURL = "#fh5co-skills";
+	} else if (sectionName == "fmw") {
+		sectionURL = "#fh5co-blog";
+	} else if (sectionName == "hireMe") {
+		sectionURL = "#fh5co-started";
+	} else if (sectionName == "contact") {
+		sectionURL = "#fh5co-consult";
+	}
+
+	var copyText = "https://ayushdayama.github.io/Portfolio" + sectionURL;
+	var tooltipMessage = "Link copied to clipboard!";
+
+	if (navigator.clipboard) {
+		navigator.clipboard.writeText(copyText)
+			.then(function () {
+				/* clipboard successfully set */
+				console.log(tooltipMessage);
+			}, function () {
+				/* clipboard write failed */
+				document.execCommand('copy');
+				console.log(tooltipMessage);
+			});
+	} else {
+		/* fallback for older browsers */
+		var textArea = document.createElement("textarea");
+		textArea.value = copyText;
+		document.body.appendChild(textArea);
+		textArea.select();
+		document.execCommand('copy');
+		console.log(tooltipMessage);
+		document.body.removeChild(textArea);
+	}
 }
